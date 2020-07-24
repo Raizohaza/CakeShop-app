ec2f914bfaf84ab65901e5a4d7f22f43e629b012
ToanPhamMimh <54280717+ToanPhamMimh@users.noreply.github.com>
2020-07-24 15:47:42 +0700

    Add project files.

---
+++ CakeShop_app/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CakeShop_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btn_ButtonOpenMenu(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+        private void btn_ButtonCloseMenu(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void btn_Aboutme(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_Source(object sender, RoutedEventArgs e)
+
+        {
+
+        }
+        private void btn_Demo(object sender, RoutedEventArgs e)
+
+        {
+
+        }
+        private void btn_Exit(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
