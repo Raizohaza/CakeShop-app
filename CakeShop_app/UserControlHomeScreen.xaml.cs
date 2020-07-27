c7414d5dd6cb65c36e18d042397c00a691a5002a
ToanPhamMimh <54280717+ToanPhamMimh@users.noreply.github.com>
2020-07-27 22:13:01 +0700

    Chuẩn bị database + UserControlHomeScreen

---
+++ CakeShop_app/UserControlHomeScreen.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CakeShop_app
+{
+    /// <summary>
+    /// Interaction logic for UserControlHomeScreen.xaml
+    /// </summary>
+    public partial class UserControlHomeScreen : UserControl
+    {
+        public UserControlHomeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var db = new CakeShop_dbEntities();
+
+            var Cakes = db.Cakes.ToList();
+            HomeListView.ItemsSource = Cakes;
+        }
+    }
+}
