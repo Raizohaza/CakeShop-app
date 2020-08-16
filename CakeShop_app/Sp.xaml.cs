c39e48e2da25a8a5fc1ffb98e5cdf87d582168f2
ToanPhamMimh <54280717+ToanPhamMimh@users.noreply.github.com>
2020-08-16 16:19:14 +0700

    them splash

---
+++ CakeShop_app/Sp.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CakeShop_app
+{
+    /// <summary>
+    /// Interaction logic for Sp.xaml
+    /// </summary>
+    public partial class Sp : Window
+    {
+        public Sp()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Next_button_Click(object sender, RoutedEventArgs e)
+        {
+            var screen = new MainWindow();
+            this.Hide();
+            screen.ShowDialog();
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
